279c406c9b42cde2f59eab01a17ba40c68f3ac27
Amandeep Kaur Sekhon <amandeepkaursekhon>
2017-11-30 19:00:51 -0500

    Initial Commit

---
+++ MAPD714-Lesson4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714-Lesson4
+//
+//  Created by Amandeep Sekhon on 2017-11-30.
+//  Copyright © 2017 Amandeep Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
